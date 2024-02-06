5824e9885d64cf07e1a10878aadc9aeb66680b84
petrykivd <petrykiv.dmytro19@gmail.com>
2024-02-06 23:01:14 +0200

    Created database engine

---
+++ db/database.py
@@ -0,0 +1,26 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+SQLALCHEMY_DATABASE_URL = (f"postgresql://"
+                           f"{os.environ['POSTGRES_USER']}:"
+                           f"{os.environ['POSTGRES_PASSWORD']}@"
+                           f"db:5432/"
+                           f"{os.environ['POSTGRES_DB']}")
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL
+)
+
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+Base = declarative_base()
